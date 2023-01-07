223de0020b3c1a526bcd85aeb5f677f5ccaed637
ftennisco <f.tennisco@hotmail.com>
2023-01-07 13:40:01 -0800

    practice work for Udacity

---
+++ whichSchool.js
@@ -0,0 +1,9 @@
+const age = 30
+
+if (age < 13) {
+  console.log("You should go to Elementary school.");
+} else if (age => 13 && age <= 18) {
+  console.log("You should go to Secondary school.");
+} else (age > 18); {
+  console.log("You should go to Lighthouse Labs.");
+}
